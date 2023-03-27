a705f0b0a54800c4420ff345413ecf91c884de39
Petar Iliev <petariliev@utexas.edu>
2023-03-26 20:09:04 -0500

    Initial Commit

---
+++ Betting-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Betting-App
+//
+//  Created by Petar Iliev on 26.3.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
